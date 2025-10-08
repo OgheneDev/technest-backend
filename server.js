1cf22b87ce074aac1a3d9ca81b249589bea22962
OgheneDev <emmanueloghene72@gmail.com>
2025-10-08 07:24:40 +0100

    CORS

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ const app = express();
 app.use(cors({
     origin: [  
         'http://localhost:3000',
-        'https://technest-frontend.vercel.app'
+        'https://technest-frontend.vercel.app',
+        'https://technest-admin.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
